b5653052eda7448f7accf7045d1355af70675315
Maksym Shchehortsov <m.shchehortsov@gmail.com>
2026-03-31 22:44:45 +0300

    fix: added script to file-uploader

--- 11-FormStyling/Assets/scripts/script.js
+++ 11-FormStyling/Assets/scripts/script.js
@@ -2,3 +2,9 @@ $(document).ready(function () {
     $(".select2").select2();
 });
 
+
+document.addEventListener('change', function (e) {
+    if (e.target.classList.contains('file-upload')) {
+        e.target.nextElementSibling.innerText = e.target.files[0].name
+    }
+})
